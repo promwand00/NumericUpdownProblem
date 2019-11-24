085cc55a02821522a93958cd1234991790f2a352
promwand <kontakt@promwand.com>
2019-11-24 20:33:46 +0100

    init:

---
+++ TestKontrolkiNumericUpDown/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestKontrolkiNumeriUpDown
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private double _rabat;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Rabat
+        {
+            get { return _rabat; }
+            set {
+                _rabat = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Rabat = 10D;
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ TestKontrolkiNumericUpDown/NumericUpDown/NumericUpDown.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestKontrolkiNumeriUpDown.NumericUpDown
+{
+    /// <summary>
+    /// Interaction logic for NumericUpDown.xaml
+    /// </summary>
+    public partial class NumericUpDown : UserControl
+    {
+        public double MinValue { get; set; } = 5D;
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double),typeof(NumericUpDown), 
+            new FrameworkPropertyMetadata(
+                0D, 
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault | FrameworkPropertyMetadataOptions.Journal,
+                OnMyPropertyChanged,
+                OnCoerceValue,
+                false,
+                UpdateSourceTrigger.Explicit
+                ));
+
+        private static object OnCoerceValue(DependencyObject d, object baseValue)
+        {
+            NumericUpDown nud = d as NumericUpDown;
+
+            if (((double)baseValue) < nud.MinValue)
+            {
+                //OnMyPropertyChanged
+                return nud.MinValue;
+            }
+            return baseValue;
+        }
+        private static void OnMyPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+            NumericUpDown numericUpDown = d as NumericUpDown;
+
+            if (numericUpDown == null)
+                return;
+
+            BindingExpression bindingExpression = numericUpDown.GetBindingExpression(NumericUpDown.ValueProperty);
+            if (bindingExpression != null)
+                bindingExpression.UpdateSource();
+
+            ((NumericUpDown)d).txtBoxValue.Text = ((double)e.NewValue).ToString();
+        }
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnUp_Click(object sender, RoutedEventArgs e)
+        {
+            Value += 100;
+        }
+
+        private void BtnDown_Click(object sender, RoutedEventArgs e)
+        {
+            Value -= 100;
+        }
+
+
+
+    }
+}
